12a44fa05c9bceacbadb7e1581eb1a43cd021e78
Ayush Jain <ayushjain2205@gmail.com>
2021-11-16 01:14:55 +0530

    Placeholder react-app added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Typewriter from "typewriter-effect";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Typewriter
+        onInit={(typewriter) => {
+          typewriter
+            .typeString("Entropy Labs")
+            .pauseFor(1000)
+            .deleteAll()
+            .typeString("Entropy Labs")
+            .pauseFor(1000)
+            .deleteAll()
+            .typeString("Entropy Labs")
+            .start();
+        }}
+      />
+      <div>
+        <h6>By Ishika Pareek and Ayush Jain</h6>
+      </div>
     </div>
   );
 }
